85fa6b2edb73ccd8ee3aa45e076b10c8832336b2
Xiaolong Wang <dragonxlwang@gmail.com>
2016-02-25 22:10:58 -0600

    script update

--- dotfiles/scripts/setup_symlinks.sh
+++ dotfiles/scripts/setup_symlinks.sh
@@ -9,7 +9,7 @@ files=(	".bash_profile"       "${HOME}"	\
         ".vimrc"              "${HOME}" \
         "ys.zsh-theme"        "${HOME}/.oh-my-zsh/themes" \
         ".zshrc"	            "${HOME}"	\
-        ".vim/colors"         "${HOME}"
+        ".vim"                "${HOME}"
       )
 
 if [[ $# -ne 1 ]]; then
@@ -28,13 +28,14 @@ ANSI_COLOR_GREEN="\033[32m"
 ANSI_COLOR_RESET="\033[0m"
 ANSI_COLOR_BLUE="\033[36m"
 
+root_dir=$(dirname $PWD)
 for i in $( seq 0 $((num_files - 1)) );
 do
   j=$(( i * 2 ))
   k=$(( j + 1 ))
   file=${files[j]}
   path=${files[k]}
-  if [[ -e ${path}/${file} ]]; 
+  if [[ -a ${path}/${file} ]]; 
   then
     echo -n "${path}/${file} already existed. Do you want to overwrite? [Y/N] "
     read ans
@@ -43,22 +44,22 @@ do
     then
       echo -e $ANSI_COLOR_RED "rm ${path}/${file}" $ANSI_COLOR_RESET 
       rm -rf ${path}/${file}
-      echo -e  $ANSI_COLOR_RED "ln -s $PWD/${1}/${file} ${path}/${file}" $ANSI_COLOR_RESET
-      ln -s $PWD/${1}/${file} ${path}/${file} 
+      echo -e  $ANSI_COLOR_RED "ln -s $root_dir/${1}/${file} ${path}/${file}" $ANSI_COLOR_RESET
+      ln -s $root_dir/${1}/${file} ${path}/${file} 
     else
-      echo -e $ANSI_COLOR_BLUE "skipping $PWD/${1}/${file}" $ANSI_COLOR_RESET
+      echo -e $ANSI_COLOR_BLUE "skipping $root_dir/${1}/${file}" $ANSI_COLOR_RESET
     fi
   else
-    echo -e $ANSI_COLOR_GREEN "ln -s $PWD/${1}/${file} ${path}/${file}" $ANSI_COLOR_RESET
-    ln -s $PWD/${1}/${file} ${path}/${file}
+    echo -e $ANSI_COLOR_GREEN "ln -s $root_dir/${1}/${file} ${path}/${file}" $ANSI_COLOR_RESET
+    ln -s $root_dir/${1}/${file} ${path}/${file}
   fi
 done    
 
 if [[ $1 == "mac" ]];
 then
-  for f in $(ls "mac/.atom");
+  for f in $(ls "$root_dir/mac/.atom");
   do
-    if [[ -e ${HOME}/.atom/$f ]];
+    if [[ -a ${HOME}/.atom/$f ]];
     then
       echo -n "${HOME}/.atom/$f already existed. Do you want to overwrite? [Y/N] "
       read ans
@@ -67,14 +68,14 @@ then
       then
         echo -e $ANSI_COLOR_RED "rm ${HOME}/.atom/$f" $ANSI_COLOR_RESET 
         rm -rf ${HOME}/.atom/$f
-        echo -e $ANSI_COLOR_RED "ln -s $PWD/mac/.atom/$f ${HOME}/.atom/$f" $ANSI_COLOR_RESET
-        ln -s $PWD/mac/.atom/$f ${HOME}/.atom/$f
+        echo -e $ANSI_COLOR_RED "ln -s $root_dir/mac/.atom/$f ${HOME}/.atom/$f" $ANSI_COLOR_RESET
+        ln -s $root_dir/mac/.atom/$f ${HOME}/.atom/$f
       else
-        echo -e $ANSI_COLOR_BLUE "skipping $PWD/mac/.atom/$f" $ANSI_COLOR_RESET
+        echo -e $ANSI_COLOR_BLUE "skipping $root_dir/mac/.atom/$f" $ANSI_COLOR_RESET
       fi
     else
-      echo -e $ANSI_COLOR_BLUE "ln -s $PWD/mac/.atom/$f ${HOME}/.atom/$f" $ANSI_COLOR_RESET
-      ln -s $PWD/mac/.atom/$f ${HOME}/.atom/$f
+      echo -e $ANSI_COLOR_BLUE "ln -s $root_dir/mac/.atom/$f ${HOME}/.atom/$f" $ANSI_COLOR_RESET
+      ln -s $root_dir/mac/.atom/$f ${HOME}/.atom/$f
     fi
   done
 fi
