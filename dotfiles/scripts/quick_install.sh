fcc8d740a7e19865f33734c34c708a9947c00ad8
Xiaolong Wang <dragonxlwang@gmail.com>
2016-04-01 12:26:17 -0500

    fonts

--- dotfiles/scripts/quick_install.sh
+++ dotfiles/scripts/quick_install.sh
@@ -100,6 +100,8 @@ brew cask install font-m-plus
 brew cask install font-clear-sans
 brew cask install font-roboto
 
+wget https://gist.githubusercontent.com/baopham/1838072/raw/616d338cea8b9dcc3a5b17c12fe3070df1b738c0/Monaco%2520for%2520Powerline.otf ~/Library/Fonts/"Monaco for Powerline.otf"
+
 ## brew cask install brackets nvalt libreoffice
 ## brew cask install sourcetree slack gisto
 ## brew cask install alfred Skitch
