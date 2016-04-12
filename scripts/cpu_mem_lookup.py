24ad4d2d8cafed4c85c04b5913cada8fba85f7a8
Xiaolong Wang <dragonxlwang@gmail.com>
2016-04-12 01:14:21 -0500

    fix

--- scripts/cpu_mem_lookup.py
+++ scripts/cpu_mem_lookup.py
@@ -12,8 +12,8 @@ if len(sys.argv) == 1:
     print u"\u24D2 {0:4}%:{1} \u24Dc {2:4}%:{3}".format(
         cpu_pct, cpu_cnt, mem_pct, mem_all).encode('utf-8')
 elif sys.argv[1] == 'cpu':
-    print u"\u24D2 {0}%:{1}".format(cpu_pct, cpu_cnt).encode('utf-8')
+    print u"\u24D2 {0:4}%:{1}".format(cpu_pct, cpu_cnt).encode('utf-8')
 elif sys.argv[1] == 'mem':
-    print u"\u24Dc {0}%:{1}".format(mem_pct, mem_all).encode('utf-8')
+    print u"\u24Dc {0:4}%:{1}".format(mem_pct, mem_all).encode('utf-8')
 else:
     print "cpu_mem_lookup [mem, cpu]"
