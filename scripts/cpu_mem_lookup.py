5cba4d0982461de99d039627a1c514b0fe9b75b9
Xiaolong Wang <dragonxlwang@gmail.com>
2016-04-12 00:46:20 -0500

    add cpu_mem_lookup

---
+++ scripts/cpu_mem_lookup.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+import psutil
+import sys
+
+cpu_pct = psutil.cpu_percent(interval=1)
+cpu_cnt = psutil.cpu_count()
+mem_pct = psutil.virtual_memory().percent
+mem_all = float(psutil.virtual_memory().total) / float(2**30)
+
+
+if len(sys.argv) == 1:
+    print u"\u24D2 {0}%:{1} \u24Dc {2}%:{3}".format(
+        cpu_pct, cpu_cnt, mem_pct, mem_all)
+elif sys.argv[1] == 'cpu':
+    print u"\u24D2 {0}%:{1}".format(cpu_pct, cpu_cnt)
+elif sys.argv[1] == 'mem':
+    print u"\u24Dc {0}%:{1}".format(mem_pct, mem_all)
+else:
+    print "cpu_mem_lookup [mem, cpu]"
