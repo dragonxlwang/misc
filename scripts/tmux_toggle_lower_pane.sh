6ae53d21dc54bc8c1f7519458e3a5220f6327bee
Xiaolong Wang <dragonxlwang@gmail.com>
2016-04-22 16:52:57 -0500

    tmux

---
+++ scripts/tmux_toggle_lower_pane.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+if [[ $(tmux list-panes | wc -l) -eq 1 ]]; then # only 1 pain e.g. vim
+  tmux split-window -c $PWD -v -l 15
+  tmux select-pane -D
+elif tmux list-panes -F "#F" | grep -q Z; then  # zoomed already
+  tmux resize-pane -Z
+  tmux select-pane -D
+elif [[ $(tmux display-message -p '#P') -eq 1 ]]; then  # in main pane
+  tmux resize-pane -Z
+else  # in small pane
+  tmux select-pane -U
+  tmux resize-pane -Z
+fi
+
