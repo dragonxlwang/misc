a599bf9ff81474378ffedbdce56b8f79c8649576
Xiaolong Wang <dragonxlwang@gmail.com>
2016-04-23 00:06:27 -0500

    tmux

--- scripts/tmux_toggle_lower_pane.sh
+++ scripts/tmux_toggle_lower_pane.sh
@@ -2,7 +2,6 @@
 
 if [[ $(tmux list-panes | wc -l) -eq 1 ]]; then # only 1 pain e.g. vim
   tmux split-window -c "#{pane_current_path}" -v -p 20
-  tmux select-pane -D
 elif tmux list-panes -F "#F" | grep -q Z; then  # zoomed already
   tmux resize-pane -Z
   tmux select-pane -D
