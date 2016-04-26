8a4e1a6139461201134d02d1c253130d424ee7cd
Xiaolong Wang <dragonxlwang@gmail.com>
2016-04-25 23:41:40 -0500

    fix tmux

--- scripts/tmux_toggle_lower_pane.sh
+++ scripts/tmux_toggle_lower_pane.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
-
+HEIGHT=$(($(tmux display-message -p '#{pane_height}') / 5))
 if [[ $(tmux list-panes | wc -l) -eq 1 ]]; then # only 1 pain e.g. vim
-  tmux split-window -c "#{pane_current_path}" -v -p 20
+  tmux split-window -c "#{pane_current_path}" -v -l $HEIGHT
 elif tmux list-panes -F "#F" | grep -q Z; then  # zoomed already
   tmux resize-pane -Z
   tmux select-pane -D
+  tmux resize-pane -y $HEIGHT
 elif [[ $(tmux display-message -p '#P') -eq 1 ]]; then  # in main pane
   tmux resize-pane -Z
 else  # in small pane
