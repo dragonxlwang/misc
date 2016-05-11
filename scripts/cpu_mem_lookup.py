7a8b5e5837deeeb573951cc5a16dd1d2f2c39512
Xiaolong Wang <dragonxlwang@gmail.com>
2016-05-11 15:27:14 -0500

    tmux

--- scripts/cpu_mem_lookup.py
+++ scripts/cpu_mem_lookup.py
@@ -9,11 +9,11 @@ mem_all = int(float(psutil.virtual_memory().total) / float(2**30))
 
 
 if len(sys.argv) == 1:
-    print u"\u24D2 {0:4}%:{1} \u24Dc {2:4}%:{3}".format(
+    print u"\u24D2 {0:04}%:{1} \u24Dc {2:04}%:{3}".format(
         cpu_pct, cpu_cnt, mem_pct, mem_all).encode('utf-8')
 elif sys.argv[1] == 'cpu':
-    print u"\u24D2 {0:4}%:{1}".format(cpu_pct, cpu_cnt).encode('utf-8')
+    print u"\u24D2 {0:04}%:{1}".format(cpu_pct, cpu_cnt).encode('utf-8')
 elif sys.argv[1] == 'mem':
-    print u"\u24Dc {0:4}%:{1}".format(mem_pct, mem_all).encode('utf-8')
+    print u"\u24Dc {0:04}%:{1}".format(mem_pct, mem_all).encode('utf-8')
 else:
     print "cpu_mem_lookup [mem, cpu]"
