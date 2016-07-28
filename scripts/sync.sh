49a509efa51250238f99a9d3512bc74bbc21511b
Xiaolong Wang <dragonxlwang@gmail.com>
2016-07-27 23:08:13 -0700

    commit

---
+++ scripts/sync.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+found=0
+curpwd=$(pwd)
+while [[ ! -a $(pwd)/.sync ]]; do
+  cd $(dirname $(pwd))
+done
+exe_path="$(pwd)/.sync"
+
+for x in $(git status -s | awk '{print $2}'); do
+  echo $(readlink -f $x)
+  $exe_path $(readlink -f $x)
+done
+
