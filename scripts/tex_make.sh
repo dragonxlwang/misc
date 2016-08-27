4ae2d30d54f8e67e57f33be7249b3185945c6d60
Xiaolong Wang <dragonxlwang@gmail.com>
2016-08-26 19:18:46 -0700

    commit

--- scripts/tex_make.sh
+++ scripts/tex_make.sh
@@ -4,12 +4,15 @@
 
 if [[ ! -a .latexmkrc ]]; then
   echo "\$pdf_previewer = \"open -a /Applications/Skim.app\";" >> .latexmkrc
-  echo "\$clean_ext = \"paux lox pdfsync out\";" >> .latexmkrc
   echo "@default_files = ('main.tex');" >> .latexmkrc
+  echo "\$pdflatex = 'pdflatex -pdf -verbose -file-line-error -synctex=1 -interaction=nonstopmode %O %S';" >> .latexmkrc
+  echo "\$clean_ext = \"paux lox pdfsync out synctex.gz\";" >> .latexmkrc
 fi
 
 if [[ $1 == "make" ]]; then
   latexmk -pdf
+elif [[ $1 == "preview" ]]; then
+  latexmk -pdf -pvc
 elif [[ $1 == 'clean' ]]; then
   latexmk -C
 fi
