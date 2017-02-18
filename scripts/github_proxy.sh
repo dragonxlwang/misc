ac8fc3945a210d87959d6d453154515c4ccbebe2
Xiaolong Wang <xlwang@fb.com>
2017-02-17 16:59:13 -0800

    proxy

---
+++ scripts/github_proxy.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+if [[ $(uname) == Darwin ]]; then
+  ssh $1
+elif [[ $(rpm -q --queryformat '%{VERSION}' centos-release) == 6* ]]; then
+  nc -X connect -x fwdproxy:8080 $1 $2
+else
+  nc --proxy-type http --proxy fwdproxy:8080 $1 $2
