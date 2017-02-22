d073287e7b8aa86b57ed8d5f0d572c8f4cd0efd3
Xiaolong Wang <xlwang@fb.com>
2017-02-22 10:27:48 -0800

    dev changes

---
+++ scripts/color_print.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env zsh
+
+# print with or without newline
+function _color_echo {
+  if [[ "$2" == "-n" ]]; then echo -ne "\033[${1}${@:3}\033[0m"
+  else echo -e "\033[${1}${@:2}\033[0m"; fi }
+function blackecho    { _color_echo "1;30m" "${@}" }
+function redecho      { _color_echo "1;31m" "${@}" }
+function greenecho    { _color_echo "1;32m" "${@}" }
+function yellowecho   { _color_echo "1;33m" "${@}" }
+function blueecho     { _color_echo "1;34m" "${@}" }
+function magentaecho  { _color_echo "1;35m" "${@}" }
+function cyanecho     { _color_echo "1;36m" "${@}" }
+function whiteecho    { _color_echo "1;37m" "${@}" }
