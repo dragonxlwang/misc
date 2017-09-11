a013798bd1083d41988e7197b36854886f3c77e1
Xiaolong Wang <xlwang@fb.com>
2017-09-11 10:53:25 -0700

    dev changes

--- scripts/vim8_dev_build.sh
+++ scripts/vim8_dev_build.sh
@@ -9,3 +9,30 @@ export https_proxy=fwdproxy:8080
 
 sudo yum update "vim*"
 # sudo rm -rf /etc/yum.repos.d/mcepl-vim8-epel-7.repo
+
+# sudo yum install -y ruby ruby-devel lua lua-devel luajit \
+#     luajit-devel ctags git python python-devel \
+#     python3 python3-devel tcl-devel \
+#     perl perl-devel perl-ExtUtils-ParseXS \
+#     perl-ExtUtils-XSpp perl-ExtUtils-CBuilder \
+#     perl-ExtUtils-Embed
+# cd ~
+# ver=7.4.1952
+# curl -OL https://github.com/vim/vim/archive/v$ver.tar.gz
+# tar -zxvf v$ver.tar.gz
+# cd vim-$ver
+# ./configure --prefix=/usr/local \
+#             --with-features=huge \
+#             --enable-multibyte \
+#             --enable-rubyinterp \
+#             --enable-pythoninterp \
+#             --with-python-config-dir=/usr/local/lib/python2.7/config \
+#             --enable-perlinterp \
+#             --enable-luainterp \
+#             --enable-gui=gtk2 --enable-cscope \
+#             LDFLAGS="-L/usr/local/lib -Wl,-rpath=/usr/local/lib"
+# make VIMRUNTIMEDIR=/usr/local/share/vim/vim74
+# make install
+# /usr/local/bin/vim --version
+# cd ~
+# rm -rf vim-$ver
--- scripts/ycm_dev_build.sh
+++ scripts/ycm_dev_build.sh
@@ -3,11 +3,27 @@
 cd ~/.vim/
 /bin/rm -rf ./bundle/YouCompleteMe
 vim +PluginInstall +qall
+cd ~/.vim/bundle/YouCompleteMe
+git reset --hard 41fb6537e290256fc3ca585bcd54860e67f1d7e5
 
 DEST_ROOT=~/.vim/bundle/YouCompleteMe/third_party/ycmd # change this directory to your heart's cotent. Keep it in local disk, however.
 git clone https://github.com/Valloric/ycmd.git $DEST_ROOT
 cd $DEST_ROOT
 git submodule update --init --recursive
+# cd $DEST_ROOT
+# git reset --hard c4beb2fb50e6268c9066e11874ead49ccb442332
+# cd $DEST_ROOT/third_party/JediHTTP
+# git reset --hard c376aadd89c7687ecf2c7a68f4cbecab3bbcd57b
+# cd $DEST_ROOT/third_party/JediHTTP/vendor/jedi
+# git reset --hard 8542047e5c92f1430bc11ec462e8ffd24e103a1d
+# cd $DEST_ROOT/third_party/JediHTTP/vendor/bottle
+# git reset --hard 7423aa0f64e381507d1e06a6bcab48888baf9a7b
+# cd $DEST_ROOT/third_party/gocode
+# git reset --hard eef10fdde96a12528a6da32f51bf638b2863a3b1
+# cd $DEST_ROOT/third_party/racerd
+# git reset --hard e3f3ff010fce2c67195750d9a6a669ffb3c2ac5f
+# cd $DEST_ROOT
+
 
 TMP_DIR=/tmp/ycm_build
 rm -rf $TMP_DIR
@@ -18,10 +34,12 @@ mkdir -p $BUILD_DIR
 
 cd $BUILD_DIR
 
-
 FBCODE_DIR="$(hg root --cwd ~/fbsource/fbcode)"/fbcode # set this
 FBCODE_PLATFORM=gcc-5-glibc-2.23
 TP2="$FBCODE_DIR"/third-party-buck/"$FBCODE_PLATFORM"/build
+PYTHON_VERSION=2.7
+PYTHON_DIR=${TP2}/python/${PYTHON_VERSION}
+
 
 # -DPATH_TO_LLVM_ROOT="$TP2"/llvm-fb \
 
@@ -39,7 +57,7 @@ env -i PATH=/bin:/usr/bin:/usr/local/bin \
   -DCMAKE_CXX_COMPILER=g++.par \
   -DCMAKE_C_FLAGS="--platform=$FBCODE_PLATFORM" \
   -DCMAKE_CXX_FLAGS="--platform=$FBCODE_PLATFORM" \
-  -DPYTHON_LIBRARY="$TP2"/python/2.7/lib/libpython2.7.so \
+  -DPYTHON_LIBRARY=${PYTHON_DIR}/lib/libpython${PYTHON_VERSION}.so \
   -DEXTERNAL_LIBCLANG_PATH="$DEST_ROOT"/libclang.so \
   . $DEST_ROOT/cpp
 
