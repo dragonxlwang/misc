5e99e1198486dee1392d2e0293e2c8c356111da2
Xiaolong Wang <xlwang@fb.com>
2017-09-14 11:44:15 -0700

    dev changes

--- scripts/vim8_dev_build.sh
+++ scripts/vim8_dev_build.sh
@@ -2,37 +2,51 @@
 
 cd ~
 
-sudo curl -L https://copr.fedorainfracloud.org/coprs/mcepl/vim8/repo/epel-7/mcepl-vim8-epel-7.repo -o /etc/yum.repos.d/mcepl-vim8-epel-7.repo
+# sudo curl -L https://copr.fedorainfracloud.org/coprs/mcepl/vim8/repo/epel-7/mcepl-vim8-epel-7.repo -o /etc/yum.repos.d/mcepl-vim8-epel-7.repo
 
-export http_proxy=fwdproxy:8080
-export https_proxy=fwdproxy:8080
+# export http_proxy=fwdproxy:8080
+# export https_proxy=fwdproxy:8080
 
-sudo yum update "vim*"
-# sudo rm -rf /etc/yum.repos.d/mcepl-vim8-epel-7.repo
+# sudo yum update "vim*"
 
-# sudo yum install -y ruby ruby-devel lua lua-devel luajit \
-#     luajit-devel ctags git python python-devel \
-#     python3 python3-devel tcl-devel \
-#     perl perl-devel perl-ExtUtils-ParseXS \
-#     perl-ExtUtils-XSpp perl-ExtUtils-CBuilder \
-#     perl-ExtUtils-Embed
-# cd ~
-# ver=7.4.1952
-# curl -OL https://github.com/vim/vim/archive/v$ver.tar.gz
-# tar -zxvf v$ver.tar.gz
-# cd vim-$ver
-# ./configure --prefix=/usr/local \
-#             --with-features=huge \
-#             --enable-multibyte \
-#             --enable-rubyinterp \
-#             --enable-pythoninterp \
-#             --with-python-config-dir=/usr/local/lib/python2.7/config \
-#             --enable-perlinterp \
-#             --enable-luainterp \
-#             --enable-gui=gtk2 --enable-cscope \
-#             LDFLAGS="-L/usr/local/lib -Wl,-rpath=/usr/local/lib"
-# make VIMRUNTIMEDIR=/usr/local/share/vim/vim74
-# make install
-# /usr/local/bin/vim --version
-# cd ~
-# rm -rf vim-$ver
+# https://github.com/Valloric/YouCompleteMe/wiki/Building-Vim-from-source
+
+sudo rm -rf /etc/yum.repos.d/mcepl-vim8-epel-7.repo
+
+sudo yum install -y ruby ruby-devel lua lua-devel luajit \
+    luajit-devel ctags git python python-devel \
+    python3 python3-devel tcl-devel \
+    perl perl-devel perl-ExtUtils-ParseXS \
+    perl-ExtUtils-XSpp perl-ExtUtils-CBuilder \
+    perl-ExtUtils-Embed
+cd ~
+ver=8.0.1098
+curl -OL https://github.com/vim/vim/archive/v$ver.tar.gz
+tar -zxvf v$ver.tar.gz
+cd ~/vim-$ver
+./configure --prefix=/usr/local \
+            --with-features=huge \
+            --enable-multibyte \
+            --enable-rubyinterp=yes \
+            --enable-pythoninterp=yes \
+            --with-python-config-dir=/local/fbcode/gcc-5-glibc-2.23/lib/python2.7/config/ \
+            # --enable-python3interp=yes \
+            # --with-python3-config-dir=/usr/local/fbcode/gcc-5-glibc-2.23/lib/python3.6/config-3.6m-fb-gcc5-x86_64/ \
+            --enable-perlinterp=yes \
+            --enable-luainterp=yes \
+            --enable-gui=gtk2 \
+            --enable-cscope
+make VIMRUNTIMEDIR=/usr/local/share/vim/vim80
+
+# sudo update-alternatives --install /usr/bin/editor editor /usr/bin/vim 1
+# sudo update-alternatives --set editor /usr/bin/vim
+# sudo update-alternatives --install /usr/bin/vi vi /usr/bin/vim 1
+# sudo update-alternatives --set vi /usr/bin/vim
+
+cd ~/vim-$ver
+sudo make install
+/usr/local/bin/vim --version
+vim --version
+cd ~
+rm -rf ~/vim-$ver
+rm -rf v$ver.tar.gz
