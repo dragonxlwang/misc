c9472ffe1b0d61f78a505a580a362fa74b46a927
Xiaolong Wang <xlwang@fb.com>
2017-11-14 08:33:58 -0800

    dev changes

--- scripts/fbcode_py_unittest_pdb.py
+++ scripts/fbcode_py_unittest_pdb.py
@@ -4,8 +4,8 @@
 ####        and support of pdb                                              ####
 ################################################################################
 
-tests = ['***'] # replace with test functions
-test_class = *** # replace with test class
+tests = ['***']   # replace with test functions
+test_class = ***  # replace with test class
 for attr in dir(test_class):
     if attr.startswith('test_') and attr not in tests:
         delattr(test_class, attr)
@@ -14,16 +14,20 @@ for attr in dir(test_class):
         delattr(test_class, attr)
 
         def test(self):
+            import sys
+            sys.stdout = file('/dev/stdout', 'w')
             try:
                 test_func(self)
             except:
-                import pdb, traceback, sys, signal
+                import pdb
+                import traceback
+                import sys
+                import signal
 
                 def signal_handler(signum, frame):
                     print("Exiting from pdb session")
                     sys.exit(signum)
 
-                sys.stdout = file('/dev/stdout', 'w')
                 type, value, tb = sys.exc_info()
                 traceback.print_exc()
                 signal.signal(signal.SIGINT, signal_handler)
