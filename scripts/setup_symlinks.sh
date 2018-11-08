d79dc214534287a8574e0bad8ec4f4f5a6958b0a
Xiaolong Wang <xlwang@fb.com>
2018-11-08 09:17:12 -0800

    dev changes

--- scripts/setup_symlinks.sh
+++ scripts/setup_symlinks.sh
@@ -139,7 +139,8 @@ files=(                                                           \
   "$root_dir/lib/.gdbinit"          "${HOME}/.gdbinit"            \
   "$root_dir/lib/.ycm_extra_conf.py"    "${HOME}/.vim/.ycm_extra_conf.py"     \
   "$root_dir/lib/.ycm_extra_conf.py"    "${HOME}/fbcode/.ycm_extra_conf.py"   \
-  "${HOME}/fbsource/fbcode"         "${HOME}/fbcode")
+  "${HOME}/fbsource/fbcode"         "${HOME}/fbcode"
+  "${HOME}/fbsource/configerator"   "${HOME}/configerator")
 
 for i in $( seq 0 $(( ${#files[@]} / 2 - 1 )) );
 do
