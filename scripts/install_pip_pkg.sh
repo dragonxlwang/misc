366b5aa4dbcd59e843deb9b425f20aae6e469e00
Xiaolong Wang <xlwang@fb.com>
2021-06-02 13:54:11 -0700

    darwin change

--- scripts/install_pip_pkg.sh
+++ scripts/install_pip_pkg.sh
@@ -2,28 +2,26 @@
 
 # pip
 alias pip=pip3
-pip install --upgrade pip
-pip install "ipython[all]" --upgrade ## --force-reinstall
-sudo -H pip install -U nltk
+pip install --user --upgrade pip
+pip install --user "ipython[all]" --upgrade ## --force-reinstall
+# sudo -H pip install -U nltk
 ## python -m nltk.downloader all
-pip install -U jupyter
-pip install -U flake8
-pip install -U pep8
-pip install -U autopep8
-pip install -U yapf
-pip install -U jedi
-pip install -U psutil
-pip install -U pysqlite
-pip install -U numpy
-pip install -U scipy
-pip install -U matplotlib
-pip install -U numba
-pip install -U pillow
-pip install -U pygame
+pip install --user  jupyter
+pip install --user flake8
+pip install --user pep8
+pip install --user autopep8
+pip install --user yapf
+pip install --user jedi
+pip install --user psutil
+pip install --user pysqlite
+pip install --user numpy
+pip install --user scipy
+pip install --user matplotlib
+pip install --user numba
+pip install --user pillow
+pip install --user pygame
 
 # pip install -U $(curl -s
 #   https://api.github.com/repos/matplotlib/basemap/releases/latest |
 #   grep zipball_url | awk '{print $2}' | cut -d \" -f2)
-pip install -U git+https://github.com/matplotlib/basemap.git
-
-
+pip install --user git+https://github.com/matplotlib/basemap.git
