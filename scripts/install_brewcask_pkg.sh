5ecbf873ab183b979914560cc38f6d6e8159b9b5
Xiaolong Wang <xlwang@fb.com>
2021-06-02 11:49:39 -0700

    darwin change

--- scripts/install_brewcask_pkg.sh
+++ scripts/install_brewcask_pkg.sh
@@ -1,41 +1,41 @@
 #!/bin/sh
 
 # homebrew caskroom/cask
-brew tap caskroom/cask
-brew tap caskroom/versions
-brew tap caskroom/fonts
+brew tap homebrew/cask
+brew tap homebrew/versions
+brew tap homebrew/fonts
 brew tap homebrew/core
 
 brew install graphviz
 
-brew cask install xquartz
-brew cask install dropbox google-drive google-chrome adium qq skype
-brew cask install spotify neteasemusic inkscape gimp
-brew cask install iterm2-beta atom filezilla the-unarchiver flux
-brew cask install mactex bibdesk texmaker bbedit adobe-acrobat-reader mou
-brew cask install cheatsheet suspicious-package
-brew cask install github-desktop
-brew cask install java tcl quip
+brew install xquartz
+brew install dropbox google-drive google-chrome adium qq skype
+brew install spotify neteasemusic inkscape gimp
+brew install iterm2-beta atom filezilla the-unarchiver flux
+brew install mactex bibdesk texmaker bbedit adobe-acrobat-reader mou
+brew install cheatsheet suspicious-package
+brew install github-desktop
+brew install java tcl quip
 
-brew cask install qlcolorcode
-brew cask install qlstephen
-brew cask install qlmarkdown
-brew cask install quicklook-json
-brew cask install qlprettypatch
-brew cask install quicklook-csv
-brew cask install betterzipql
-brew cask install webpquicklook
-brew cask install skim
+brew install qlcolorcode
+brew install qlstephen
+brew install qlmarkdown
+brew install quicklook-json
+brew install qlprettypatch
+brew install quicklook-csv
+brew install betterzipql
+brew install webpquicklook
+brew install skim
 
-brew cask install font-inconsolata
-brew cask install font-source-code-pro
-brew cask install font-profontx
-brew cask install font-andale-mono
-brew cask install font-droid-sans-mono
-brew cask install font-dejavu-sans-mono-for-powerline
-brew cask install font-m-plus
-brew cask install font-clear-sans
-brew cask install font-roboto
+brew install font-inconsolata
+brew install font-source-code-pro
+brew install font-profontx
+brew install font-andale-mono
+brew install font-droid-sans-mono
+brew install font-dejavu-sans-mono-for-powerline
+brew install font-m-plus
+brew install font-clear-sans
+brew install font-roboto
 
 ## brew cask install brackets nvalt libreoffice
 ## brew cask install sourcetree slack gisto
