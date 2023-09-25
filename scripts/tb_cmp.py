0a0e9c2b45761864d569868a4fea91fab8cf899f
Xiaolong Wang <xlwang@fb.com>
2023-09-25 11:57:19 -0700

    dev changes

--- scripts/tb_cmp.py
+++ scripts/tb_cmp.py
@@ -3,11 +3,15 @@
 import click
 
 
-@click.command()
+@click.group()
+def main() -> None:
+    pass
+
+
+@main.command()
 @click.argument("runs", required=True, nargs=-1, type=str)
 def gen(runs: str) -> None:
     url = "https://www.internalfb.com/intern/tensorboard/?dir="
-    """Simple program that greets NAME for a total of COUNT times."""
     parts = []
     for i, r in enumerate(runs):
         if "," in r:
@@ -18,6 +22,19 @@ def gen(runs: str) -> None:
     url = url + ",".join(parts)
     print(url)
 
+@main.command()
+@click.argument("runs", required=True, nargs=-1, type=str)
+def gen_dr(runs: str) -> None:
+    url = "https://www.internalfb.com/intern/tensorboard/?dir="
+    parts = []
+    for i, r in enumerate(runs):
+        if "," in r:
+            a, r = r.split(",")
+        else:
+            a = i
+        parts += [f"{a}:manifold://deep_retrieval/tree/jobs/{r}/tensorboard"]
+    url = url + ",".join(parts)
+    print(url)
 
 if __name__ == "__main__":
-    gen()
+    main()
