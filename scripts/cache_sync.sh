9646fda05ede1c52a00dffa32b3f8b53cbf794d5
Xiaolong Wang <xlwang@fb.com>
2024-04-23 22:56:02 -0700

    dev cache sync

--- scripts/cache_sync.sh
+++ scripts/cache_sync.sh
@@ -6,7 +6,7 @@ ANSI_COLOR_RESET="\033[0m"
 ANSI_COLOR_BLUE="\033[36m"
 ANSI_COLOR_YELLOW="\033[33m"
 
-root_dir=$(dirname $(dirname $(greadlink -f $0)))
+root_dir=$(dirname $(dirname $(greadlink -f $0)) 2>/dev/null )
 [[ $? -ne 0 ]] && root_dir=$(dirname $(dirname $(readlink -f $0)))
 
 echo $root_dir
