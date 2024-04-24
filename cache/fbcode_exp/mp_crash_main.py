192a28e693920a785963ff1428483eafffb53305
Xiaolong Wang <xlwang@fb.com>
2024-04-23 22:50:22 -0700

    dev cache sync

---
+++ cache/fbcode_exp/mp_crash_main.py
@@ -0,0 +1,49 @@
+# (c) Meta Platforms, Inc. and affiliates. Confidential and proprietary.
+
+import multiprocessing
+
+import click
+from scripts.xlwang.mp_crash import main
+
+
+def good_main():
+    try:
+        main()
+    except Exception as e:
+        print(f"re-raise Exception {e}")
+        raise e
+    finally:
+        # Get all active children
+        print("Going to terminate all child processes")
+        children = multiprocessing.active_children()
+
+        # Kill all active children
+        for child in children:
+            print(f"Going to terminate a child process. PID: {child.pid}")
+            child.kill()
+        print("All child processes are terminated.")
+
+
+def bad_main():
+    try:
+        main()
+    except Exception as e:
+        print(f"re-raise Exception {e}")
+        raise e
+
+
+@click.command
+@click.option(
+    "--bad",
+    is_flag=True,
+    default=False,
+)
+def crash_main(bad: bool):
+    if bad:
+        bad_main()
+    else:
+        good_main()
+
+
+if __name__ == "__main__":
+    crash_main()
---
+++ scripts/cache_sync.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+ANSI_COLOR_RED="\034[31m"
+ANSI_COLOR_GREEN="\033[32m"
+ANSI_COLOR_RESET="\033[0m"
+ANSI_COLOR_BLUE="\033[36m"
+ANSI_COLOR_YELLOW="\033[33m"
+
+root_dir=$(dirname $(dirname $(greadlink -f $0)))
+[[ $? -ne 0 ]] && root_dir=$(dirname $(dirname $(readlink -f $0)))
+
+echo $root_dir
+
+
+dst_dir="${root_dir}/cache"
+
+## darwin & linux suite
+files=(                                                                 \
+  "${HOME}/.z"                      "${dst_dir}/.z"                     \
+  "${HOME}/.zsh_history"            "${dst_dir}/.zsh_history"           \
+  "${HOME}/.viminfo"                "${dst_dir}/.viminfo"               \
+  "${HOME}/.cache/ctrlp/."          "${dst_dir}/ctrlp/"                 \
+  "${HOME}/fbcode/scripts/xlwang/"  "${dst_dir}/fbcode_exp/"            \
+)
+
+mkdir -p $dst_dir
+
+for i in $( seq 0 $(( ${#files[@]} / 2 - 1 )) );
+do
+  j=$(( i * 2 ))
+  k=$(( j + 1 ))
+  src=${files[j]}
+  dst=${files[k]}
+  if [[ -e $src ]] ; then
+    mkdir -p $(dirname $dst)
+    cp -rL $src $dst
+    echo -e $ANSI_COLOR_GREEN"cp -r $src $dst"$ANSI_COLOR_RESET
+  else
+    echo -e $ANSI_COLOR_RED"$src file does not exists!"$ANSI_COLOR_RESET
+  fi
+done
+
+cd $root_dir
+git commit --verbose --all -m "dev cache sync"
+git pull --rebase origin
+git push origin
+
+echo "Finished!"
